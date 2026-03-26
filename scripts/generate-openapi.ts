ef775e9822a9504c37987301f6c9dec9000bbc0f
Sharhan Kaji <shabibbhaikaji@rrc.ca>
2026-03-26 15:54:56 -0500

    feat: create script to generate OpenAPI specification

---
+++ scripts/generate-openapi.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import fs from "fs";
+import { generateSwaggerSpec } from "../config/swaggerOptions";
+
+const specs = generateSwaggerSpec();
+fs.writeFileSync("./openapi.json", JSON.stringify(specs, null, 2));
+console.log("OpenAPI specification generated successfully!");
